d4224a8fede4e5e429dc2a36f1e3c1363be2104e
Pinto Júnior <junior_pinto@hotmail.com>
2022-03-14 22:30:24 -0300

    ok

---
+++ aula16/funcao02.js
@@ -0,0 +1,4 @@
+function soma(n1=0, n2=0) { //quando não passado o valor, a função e executada com valor predefinido na função, aqui no caso é zero.
+    return n1 + n2;
+}
+console.log(soma(2, 5));
